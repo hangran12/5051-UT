3f220fd50cec3d523114adb3ddba9f556e514e95
Hannah Grandine <grandinehann@seattleu.edu>
2019-06-04 14:58:37 -0700

    Added ReportViewModelTests.cs to the project

---
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ReportViewModelTests
+    {
+        [TestMethod]
+        public void ReportViewModel_Instantiate_Default_Should_Pass()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
